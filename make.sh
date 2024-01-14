ee83e0853d06495568fec0074a4d3afa34242c15
root <root@DESKTOP-LSPHR48>
2024-01-14 13:01:20 +0900

    update: clean

--- make.sh
+++ make.sh
@@ -2,6 +2,10 @@
 # git clone https://github.com/jhkim19940830/llvm-project	#llvm-dep
 # git clone --depth 1 https://github.com/google/oss-fuzz.git 	#origin
 git clone --depth 1 https://github.com/jhkim19940830/oss-fuzz
-cp -r oss-fuzz/projects cwlab/
+cd oss-fuzz
+mv ./* ../
+cd ..
+rm -r oss-fuzz
+cp -r projects cwlab/
 cd oss-fuzz/infra/base-images/base-clang
-docker build . -t gcr.io/oss-fuzz-base/base-clang
+docker build . -t gcr.io/oss-fuzz-base/base-clang		#custom image build
